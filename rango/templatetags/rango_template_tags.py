60a478593670d5211cde912a5481bafa1748b5af
Xinli Wang <2519768W@student.gla.ac.uk>
2021-07-29 23:44:48 +0100

    exercise finished but did not test and run

--- rango/templatetags/rango_template_tags.py
+++ rango/templatetags/rango_template_tags.py
@@ -3,5 +3,6 @@ from django.template.defaultfilters import register
 from rango.models import Category
 
 @register.inclusion_tag('rango/categories.html')
-def get_catgory_list():
-    return {'categories':Category.objects.all()}
+def get_catgory_list(current_catrgory=None):
+    return {'categories':Category.objects.all(),
+            'current_category':current_catrgory}
