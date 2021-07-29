4951b830406231a3e5a36113cd3df027f74d9be8
Xinli Wang <2519768W@student.gla.ac.uk>
2021-07-30 00:19:19 +0100

    After test and everythind OK (chapter8 finished)

--- rango/templatetags/rango_template_tags.py
+++ rango/templatetags/rango_template_tags.py
@@ -1,8 +1,9 @@
 from django import template
-from django.template.defaultfilters import register
 from rango.models import Category
 
+register = template.Library()
+
 @register.inclusion_tag('rango/categories.html')
-def get_catgory_list(current_catrgory=None):
+def get_category_list(current_catrgory=None):
     return {'categories':Category.objects.all(),
             'current_category':current_catrgory}
